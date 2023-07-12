cf24d3b7c6e3156dd69c10ed1d69f12ce07bffa1
USERSAD\EDALLOC3I <claudio11.dallospedale@nttdata.com>
2023-07-12 16:31:26 +0200

    Recover Project

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-<<<<<<< Updated upstream
-const { i18n } = require('./next-i18next.config')
-
-const nextConfig = {
-  reactStrictMode: true,
-  i18n,
-}
-=======
 const nextConfig = {}
->>>>>>> Stashed changes
 
 module.exports = nextConfig
