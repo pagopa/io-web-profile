a81d9a092480d76969e359717bd887fc48247060
USERSAD\EDALLOC3I <claudio11.dallospedale@nttdata.com>
2023-07-14 09:03:34 +0200

    editing feedBackMessage column style

--- src/app/[locale]/_component/feedbackMessage/feedbackMessage.tsx
+++ src/app/[locale]/_component/feedbackMessage/feedbackMessage.tsx
@@ -1,62 +1,65 @@
 import React from 'react';
-import { Typography, Grid, Button } from '@mui/material';
+import { Typography, Grid, Button, Box } from '@mui/material';
+import Link from 'next/link';
 
 interface IntroductionProps {
   topIcon?: JSX.Element;
   title: string;
   summary: string | JSX.Element;
-  button: ButtonProperty;
-}
-
-interface ButtonProperty {
-  onClick(): void;
-  isVisible: boolean;
-  variant: 'text' | 'outlined' | 'contained';
-  text: string;
+  button: {
+    href: string;
+    isVisible: boolean;
+    variant: 'text' | 'outlined' | 'contained';
+    text: string;
+  };
 }
 
 export function FeedbackMessage({ topIcon, title, summary, button }: IntroductionProps) {
   return (
     <>
-      <Grid container>
-        <Grid item xs={12} maxWidth="100%" textAlign={'center'}>
-          {topIcon}
+      <Box sx={{ maxWidth: 448, margin: '0 auto' }}>
+        <Grid container>
+          <Grid item xs={12} textAlign={'center'} pb={2}>
+            {topIcon}
+          </Grid>
+          <Grid item xs={12} pb={2}>
+            <Typography
+              fontSize={32}
+              fontWeight={700}
+              py={0}
+              px={0}
+              color="textPrimary"
+              sx={{
+                textAlign: 'center',
+              }}
+            >
+              {title}
+            </Typography>
+          </Grid>
+          <Grid item xs={12} pb={2}>
+            <Typography
+              variant="subtitle1"
+              fontSize={18}
+              fontWeight={400}
+              py={0}
+              px={0}
+              color="textPrimary"
+              sx={{
+                textAlign: 'center',
+              }}
+            >
+              {summary}
+            </Typography>
+          </Grid>
+          <Grid item xs={12} textAlign={'center'}>
+            {button.isVisible ? (
+              <Link href={button.href}>
+                <Button variant={button.variant}>{button.text || 'Missing Text'}</Button>
+              </Link>
+            ) : null}
+          </Grid>
         </Grid>
-        <Grid item xs={12} maxWidth="100%">
-          <Typography
-            fontSize={32}
-            fontWeight={700}
-            py={0}
-            px={0}
-            color="textPrimary"
-            sx={{
-              textAlign: 'center',
-            }}
-          >
-            {title}
-          </Typography>
-        </Grid>
-        <Grid item xs={12} maxWidth="100%">
-          <Typography
-            variant="subtitle1"
-            py={1}
-            px={0}
-            color="textPrimary"
-            sx={{
-              textAlign: 'center',
-            }}
-          >
-            {summary}
-          </Typography>
-        </Grid>
-        <Grid item xs={12} textAlign={'center'}>
-          {button.isVisible ? (
-            <Button onClick={() => button.onClick()} variant={button.variant}>
-              {button.text || 'Missing Text'}
-            </Button>
-          ) : null}
-        </Grid>
-      </Grid>
+      </Box>
     </>
   );
 }
