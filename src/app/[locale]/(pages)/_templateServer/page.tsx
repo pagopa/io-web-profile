29df5f6de11cf24440a800ae994db29869acf3ec
Oraldo Doci <oraldo.doci@nttdata.com>
2023-07-18 11:40:02 +0200

    adjustments for async middleware next translation and created template page for using translations in async components

---
+++ src/app/[locale]/(pages)/_templateServer/page.tsx
@@ -0,0 +1,18 @@
+import { getTranslator } from 'next-intl/server';
+
+interface LocaleProps {
+  params: {
+    readonly locale: string;
+  };
+}
+
+const Index = async ({ params: { locale } }: LocaleProps): Promise<React.ReactElement> => {
+  const t = await getTranslator(locale, 'home');
+  return (
+    <div>
+      <h1>{t('template')}</h1>
+    </div>
+  );
+};
+
+export default Index;
