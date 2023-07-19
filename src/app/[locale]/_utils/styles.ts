670c6feafe994878102cd1abcd3d45e3fb8db8e6
Oraldo Doci <oraldo.doci@nttdata.com>
2023-07-19 15:00:12 +0200

    profile block page

--- src/app/[locale]/_utils/styles.ts
+++ src/app/[locale]/_utils/styles.ts
@@ -5,3 +5,16 @@ export const commonBackground = {
     sm: 20,
   },
 };
+
+export const commonBackgroundWithBack = {
+  backgroundColor: 'background.default',
+  paddingTop: '34px',
+  paddingBottom: {
+    xs: 2,
+    sm: 10,
+  },
+  paddingX: {
+    xs: 2,
+    sm: 20,
+  },
+};
