6961f1190067c292714ede2f287daa2b39e3cf43
claudiodall <139151141+claudiodall@users.noreply.github.com>
2023-07-26 16:15:15 +0200

    // FIXME: Jira ticket number 521

--- src/app/[locale]/(pages)/validateSession/page.tsx
+++ src/app/[locale]/(pages)/validateSession/page.tsx
@@ -9,6 +9,7 @@ import { ROUTES } from '../../_utils/routes';
 const Check = (): React.ReactElement => {
   useEffect(() => {
     if (parseJwt(extractToken())) {
+      // FIXME: Jira ticket number 521
       storageTokenOps.write(extractToken());
       storageUserOps.write(userFromJwtToken(extractToken()));
       redirect(ROUTES.SESSION);
