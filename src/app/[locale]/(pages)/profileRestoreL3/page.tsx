52d45217a30443ae6f26fec88edad0fa726038b2
pelliccm <matteo.pelliccione@nttdata.com>
2023-07-27 11:04:02 +0200

    [IOPID-537] l3 pages tl

--- src/app/[locale]/(pages)/profileRestoreL3/page.tsx
+++ src/app/[locale]/(pages)/profileRestoreL3/page.tsx
@@ -1,37 +1,38 @@
 'use client';
 import { Button, Grid } from '@mui/material';
+import { useTranslations } from 'next-intl';
 import Link from 'next/link';
 import { FAQ } from '../../_component/accordion/faqDefault';
 import { BackButton } from '../../_component/backButton/backButton';
 import { IdpListOnApp } from '../../_component/idpListOnApp/idpListOnApp';
 import { Introduction } from '../../_component/introduction/introduction';
-import { commonBackgroundLightWithBack } from '../../_utils/styles';
 import { ROUTES } from '../../_utils/routes';
+import { commonBackgroundLightWithBack } from '../../_utils/styles';
 
-const RestoreProfile = (): React.ReactElement => (
-  <>
-    <Grid sx={commonBackgroundLightWithBack} xs={12} sm={12}>
-      <BackButton />
-      <Introduction
-        title={"Vuoi ripristinare l'accesso a IO?"}
-        summary={
-          "Se hai messo in sicurezza la tua identità digitale puoi ripristinare l'accesso a IO e entrare nuovamente in app con tutte le tue identità."
-        }
-        summaryColumns={{ xs: 12, md: 7.5 }}
-      />
+const RestoreProfile = (): React.ReactElement => {
+  const t = useTranslations('ioesco');
+  return (
+    <>
+      <Grid sx={commonBackgroundLightWithBack} xs={12} sm={12}>
+        <BackButton />
+        <Introduction
+          title={t('restore.restoreaccess')}
+          summary={t('restore.login-1')}
+          summaryColumns={{ xs: 12, md: 7.5 }}
+        />
 
-      <Grid container flexDirection={'column'}>
-        <IdpListOnApp />
-        <Grid item sm={10} md={7} />
-        <Link href={ROUTES.RESTORE_THANK_YOU}>
-          <Button variant="contained" size="medium">
-            Ripristina accesso a IO
-          </Button>
-        </Link>
+        <Grid container flexDirection={'column'}>
+          <IdpListOnApp />
+          <Grid item sm={10} md={7} />
+          <Link href={ROUTES.RESTORE_THANK_YOU}>
+            <Button variant="contained" size="medium">
+              {t('common.restoreioaccess')}
+            </Button>
+          </Link>
+        </Grid>
       </Grid>
-    </Grid>
-    <FAQ flow="RESTORE" />
-  </>
-);
-
+      <FAQ flow="RESTORE" />
+    </>
+  );
+};
 export default RestoreProfile;
