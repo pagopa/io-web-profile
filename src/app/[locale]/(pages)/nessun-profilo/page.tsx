7e89ea766f7abe9b90bb245371062c7419f654d2
USERSAD\EDALLOC3I <claudio11.dallospedale@nttdata.com>
2023-08-02 14:36:26 +0200

    Changing Scaffolding

--- src/app/[locale]/(pages)/nessun-profilo/page.tsx
+++ src/app/[locale]/(pages)/nessun-profilo/page.tsx
@@ -4,8 +4,8 @@ import AppleIcon from '@mui/icons-material/Apple';
 import { Button, Grid } from '@mui/material';
 import { useTranslations } from 'next-intl';
 import Link from 'next/link';
-import { Introduction } from '../_component/introduction/introduction';
-import { commonBackground } from '../_utils/styles';
+import { Introduction } from '../../_component/introduction/introduction';
+import { commonBackground } from '../../_utils/styles';
 import PlayStoreIcon from '@/app/[locale]/_icons/playstore';
 
 const L2NoProfile = (): React.ReactElement => {
