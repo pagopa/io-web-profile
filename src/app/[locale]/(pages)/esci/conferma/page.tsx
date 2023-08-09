7c6ea6c17c45060ad6ad0be2e776bf65e06d721d
Oraldo Doci <oraldo.doci@nttdata.com>
2023-08-09 17:42:27 +0200

    created session existing common component for logout flow

--- src/app/[locale]/(pages)/esci/conferma/page.tsx
+++ src/app/[locale]/(pages)/esci/conferma/page.tsx
@@ -1,97 +1,12 @@
 'use client';
 
-import { Box, Button, Card, Grid, Typography } from '@mui/material';
-import { IllusSms } from '@pagopa/mui-italia';
 import { useTranslations } from 'next-intl';
-import { FAQ } from '../../../_component/accordion/faqDefault';
-import { Introduction } from '../../../_component/introduction/introduction';
-import { ROUTES } from '../../../_utils/routes';
-import { commonBackgroundLight } from '../../../_utils/styles';
-import { WebProfileApi } from '@/api/webProfileApiClient';
-import useLocalePush from '@/app/[locale]/_hooks/useLocalePush';
+import SessionExistingComp from '@/app/[locale]/_component/sessionExistingComp/sessionExistingComp';
 
 const Session = (): React.ReactElement => {
   const t = useTranslations('ioesco');
-  const pushWithLocale = useLocalePush();
-  const handleLogout = () => {
-    WebProfileApi.logoutFromIOApp()
-      .then(() => {
-        pushWithLocale(ROUTES.THANK_YOU);
-      })
-      .catch((_err) => {
-        pushWithLocale(ROUTES.LOGOUT_KO);
-      });
-  };
-  return (
-    <>
-      <Grid sx={commonBackgroundLight} container>
-        <Grid item xs={12} justifySelf={'center'}>
-          <Introduction
-            title={t('profilelogout.logout')}
-            summary={
-              <span>
-                {t.rich('lplogoutpostlogin.activesession', {
-                  deviceModel: 'iPhone 12 Pro',
-                  strong: (chunks) => <strong>{chunks}</strong>,
-                })}
-              </span>
-            }
-            summaryColumns={{ xs: 12, md: 6 }}
-          />
-        </Grid>
 
-        <Grid item xs={12} justifySelf={'center'}>
-          <Card
-            sx={{
-              display: 'flex',
-              p: 2,
-            }}
-          >
-            <Box>
-              <IllusSms size={72} />
-            </Box>
-            <Box sx={{ ml: 2 }} alignSelf={'center'}>
-              <Box>
-                <Typography
-                  fontSize={18}
-                  fontWeight={700}
-                  py={0}
-                  px={0}
-                  color="textPrimary"
-                  sx={{
-                    textAlign: 'left',
-                  }}
-                >
-                  <strong>{t('common.devicemodel', { deviceModel: 'iPhone 12 Pro' })}</strong>
-                </Typography>
-              </Box>
-              <Box>
-                <Typography
-                  fontSize={14}
-                  fontWeight={400}
-                  py={0}
-                  px={0}
-                  color="textPrimary"
-                  sx={{
-                    textAlign: 'left',
-                  }}
-                >
-                  {t('lplogoutpostlogin.lastdateactivation', { date: '01/01/2022' })}
-                </Typography>
-              </Box>
-            </Box>
-          </Card>
-        </Grid>
-
-        <Grid item xs={12} mt={4}>
-          <Button sx={{ mr: 2 }} variant="contained" onClick={handleLogout}>
-            {t('common.logout')}
-          </Button>
-        </Grid>
-      </Grid>
-      <FAQ />
-    </>
-  );
+  return <SessionExistingComp title={t('profilelogout.logout')} showArrowBackBtn={true} />;
 };
 
 export default Session;
--- src/app/[locale]/(pages)/esci/sessione-attiva/loading.tsx
+++
@@ -1,5 +0,0 @@
-import Loader from '../../../_component/loader/loader';
-
-export default function Loading() {
-  return <Loader />;
-}
