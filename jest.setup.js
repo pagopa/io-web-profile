50e9ec22074fb0ab87040b4cd207ea1499386450
Claudio D <139151141+claudiodall@users.noreply.github.com>
2023-08-23 11:50:39 +0200

    [IOPID-638] : Starting with unit tests (#22)
    ## Short description
    
    Some unit tests have been added using Jest for several pages in order to
    provide adequate coverage.
    
    ## How to test
    
    To test, run yarn test to execute the tests, and yarn test --coverage to
    see the coverage.
    
    ---------
    
    Co-authored-by: Oraldo Doci <oraldo.doci@nttdata.com>

--- jest.setup.js
+++ jest.setup.js
@@ -3,4 +3,83 @@
 
 // Used for __tests__/testing-library.js
 // Learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom/extend-expect'
+import '@testing-library/jest-dom/extend-expect';
+import { useRouter } from 'next-intl/client';
+
+jest.mock('next-intl/client', () => ({
+  useRouter: jest.fn(),
+  usePathname: () => jest.fn(),
+}));
+
+export const pushMock = jest.fn();
+
+beforeEach(() => {
+  useRouter.mockReturnValue({
+    push: pushMock,
+  });
+});
+
+// Mock Link component globally
+jest.mock('next-intl/link', () => {
+  return {
+    __esModule: true,
+    default: ({ children }) => children, // Mocking Link as a simple wrapper around its children
+  };
+});
+
+// Mock localStorage
+
+const localStorageMock = (() => {
+  let store = {};
+
+  return {
+    getItem: (key) => store[key] || null,
+    setItem: (key, value) => {
+      store[key] = value;
+    },
+    removeItem: (key) => {
+      delete store[key];
+    },
+    clear: () => {
+      store = {};
+    },
+  };
+})();
+
+// Mock SessionStorage
+
+const sessionStorageMock = (() => {
+  let store = {};
+
+  return {
+    getItem: (key) => store[key] || null,
+    setItem: (key, value) => {
+      store[key] = value;
+    },
+    removeItem: (key) => {
+      delete store[key];
+    },
+    clear: () => {
+      store = {};
+    },
+  };
+})();
+
+beforeAll(() => {
+  // Mock localStorage
+  jest.spyOn(window, 'localStorage', 'get').mockImplementation(() => localStorageMock);
+  // Mock SessionStorage
+  jest.spyOn(window, 'sessionStorage', 'get').mockImplementation(() => sessionStorageMock);
+});
+
+afterEach(() => {
+  // Clear All mocks before each test
+  localStorageMock.clear();
+  sessionStorageMock.clear();
+});
+
+// Mock the _utils/common module
+jest.mock('./src/app/[locale]/_utils/common', () => ({
+  ...jest.requireActual('./src/app/[locale]/_utils/common'),
+  localeFromStorage: 'it',
+}));
