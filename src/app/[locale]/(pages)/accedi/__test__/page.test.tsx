e034d5f1fa6db9e49f75712fb41b7901f5a254d0
USERSAD\EDALLOC3I <claudio11.dallospedale@nttdata.com>
2023-08-23 11:16:19 +0200

    Fixxin test dicitonaries for PR Review

--- src/app/[locale]/(pages)/accedi/__test__/page.test.tsx
+++ src/app/[locale]/(pages)/accedi/__test__/page.test.tsx
@@ -1,12 +1,13 @@
 import { fireEvent, screen } from '@testing-library/react';
 import Access from '../page';
+import * as it from '../../../../../dictionaries/it.json';
 import { renderWithProviders } from '@/app/[locale]/_utils/test-utils';
 
 describe('test suite for Access component', () => {
   test('should render Londing in case of ', async () => {
     await renderWithProviders(<Access />);
 
-    const exitButtonL1 = await screen.findByText('Esci da IO');
+    const exitButtonL1 = await screen.findByText(it.ioesco.common.logout);
     fireEvent.click(exitButtonL1);
   });
 });
--- src/app/[locale]/(pages)/accedi/errore/__test__/errore.test.tsx
+++ src/app/[locale]/(pages)/accedi/errore/__test__/errore.test.tsx
@@ -2,9 +2,9 @@ import { useSearchParams } from 'next/navigation';
 import { fireEvent, screen } from '@testing-library/react';
 import LoginErrorPage from '../page';
 import { pushMock } from '../../../../../../../jest.setup';
+import * as it from '../../../../../../dictionaries/it.json';
 import { renderWithProviders } from '@/app/[locale]/_utils/test-utils';
 import { ROUTES } from '@/app/[locale]/_utils/routes';
-
 // Mock the useSearchParams function
 jest.mock('next/navigation', () => ({
   useSearchParams: jest.fn(),
@@ -21,63 +21,49 @@ describe('test suite for access error', () => {
   test('should render "Generic error" when errorCode is null', async () => {
     mockSearchParams.get.mockReturnValue(null);
     await renderWithProviders(<LoginErrorPage />);
-    const errorSummary = screen.getByText(
-      'Si è verificato un errore durante il login, si prega di riprovare!'
-    );
+    const errorSummary = screen.getByText(it.ioesco.error.loginerrorretry);
     expect(errorSummary).toBeInTheDocument();
   });
 
   test('should render "Credential error" for errorCode 19', async () => {
     mockSearchParams.get.mockReturnValue('19');
     await renderWithProviders(<LoginErrorPage />);
-    const errorSummary = screen.getByText(
-      'Hai inserito troppe volte un nome utente o una password non corretti: riprova fra qualche minuto o contatta il tuo fornitore di identità SPID per cambiare le tue credenziali.'
-    );
+    const errorSummary = screen.getByText(it.ioesco.error.credentialerror);
     expect(errorSummary).toBeInTheDocument();
   });
 
   test('should render "Two-factor need" for errorCode 20', async () => {
     mockSearchParams.get.mockReturnValue('20');
     await renderWithProviders(<LoginErrorPage />);
-    const errorSummary = screen.getByText(
-      'Per motivi di sicurezza è richiesto un login con un secondo fattore di autenticazione: contatta il tuo fornitore di identità SPID per capire come si usa.'
-    );
+    const errorSummary = screen.getByText(it.ioesco.error.twofactorneed);
     expect(errorSummary).toBeInTheDocument();
   });
 
   test('should render "Session Expired" for errorCode 21', async () => {
     mockSearchParams.get.mockReturnValue('21');
     await renderWithProviders(<LoginErrorPage />);
-    const errorSummary = screen.getByText(
-      "È passato troppo tempo da quando hai iniziato l'accesso: per piacere riparti dall'inizio."
-    );
+    const errorSummary = screen.getByText(it.ioesco.error.sessionexpired);
     expect(errorSummary).toBeInTheDocument();
   });
 
   test('should render "Portal Consents" for errorCode 22', async () => {
     mockSearchParams.get.mockReturnValue('22');
     await renderWithProviders(<LoginErrorPage />);
-    const errorSummary = screen.getByText(
-      'Per accedere al portale, devi acconsentire all’invio di alcuni dati. Entra di nuovo e dai il tuo consenso.'
-    );
+    const errorSummary = screen.getByText(it.ioesco.error.portalconsents);
     expect(errorSummary).toBeInTheDocument();
   });
 
   test('should render "Spid Revoked" for errorCode 23', async () => {
     mockSearchParams.get.mockReturnValue('23');
     await renderWithProviders(<LoginErrorPage />);
-    const errorSummary = screen.getByText(
-      'La tua identità SPID risulta sospesa o revocata. Per riattivare il tuo account, contatta il tuo fornitore di identità SPID.'
-    );
+    const errorSummary = screen.getByText(it.ioesco.error.spidrevoked);
     expect(errorSummary).toBeInTheDocument();
   });
 
   test('should render "Cancel Login" for errorCode 25 and click on cancel button', async () => {
     mockSearchParams.get.mockReturnValue('25');
     await renderWithProviders(<LoginErrorPage />);
-    const errorSummary = screen.getByText(
-      "Hai annullato l'operazione di login: puoi riprovare quando vuoi."
-    );
+    const errorSummary = screen.getByText(it.ioesco.error.cancellogin);
     expect(errorSummary).toBeInTheDocument();
 
     const mockHistoryBack = jest.spyOn(window.history, 'back');
@@ -92,10 +78,10 @@ describe('test suite for access error', () => {
     mockSearchParams.get.mockReturnValue('1001');
     await renderWithProviders(<LoginErrorPage />);
 
-    const errorSummary = screen.getByText("Accesso negato: non hai l'età minima richiesta.");
+    const errorSummary = screen.getByText(it.ioesco.error.minage);
     expect(errorSummary).toBeInTheDocument();
 
-    const backToHomeBtn = screen.getByText('Torna alla home');
+    const backToHomeBtn = screen.getByText(it.ioesco.common.backtohome);
     fireEvent.click(backToHomeBtn);
     expect(backToHomeBtn).toBeInTheDocument();
     expect(pushMock).toHaveBeenCalledWith(process.env.NEXT_PUBLIC_URL_IO, { locale: 'it' });
@@ -104,12 +90,10 @@ describe('test suite for access error', () => {
   test('should render "Login Error Retry" in case of not recognized code and click on button retry', async () => {
     mockSearchParams.get.mockReturnValue('1234');
     await renderWithProviders(<LoginErrorPage />);
-    const errorSummary = screen.getByText(
-      'Si è verificato un errore durante il login, si prega di riprovare!'
-    );
+    const errorSummary = screen.getByText(it.ioesco.error.loginerrorretry);
     expect(errorSummary).toBeInTheDocument();
 
-    const retryBtn = screen.getByText('Riprova');
+    const retryBtn = screen.getByText(it.ioesco.error.retry);
     fireEvent.click(retryBtn);
     expect(retryBtn).toBeInTheDocument();
     expect(pushMock).toHaveBeenCalledWith(ROUTES.LOGIN, { locale: 'it' });
