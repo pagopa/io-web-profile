533e7dbec5f4b6187cac056fc264e02817c125b3
Claudio D <139151141+claudiodall@users.noreply.github.com>
2023-09-13 09:54:57 +0200

    [IOPID-730] : Adding test to reach 80% coverage (#42)
    ## Short description
    
    The goal of this task is to achieve test coverage of 80%.
    
    ## How to test
    
    run **yarn run test --coverage**
    
    ---------
    
    Co-authored-by: Fabio Bombardi <16268789+shadowsheep1@users.noreply.github.com>

---
+++ src/app/[locale]/_component/idpListOnApp/__test__/idpListOnApp.test.tsx
@@ -0,0 +1,25 @@
+import { useSearchParams } from 'next/navigation';
+import { screen } from '@testing-library/react';
+import * as it from '../../../../../dictionaries/it.json';
+import { IdpListOnApp } from '../idpListOnApp';
+import { renderWithProviders } from '@/app/[locale]/_utils/test-utils';
+// Mock the useSearchParams function
+jest.mock('next/navigation', () => ({
+  useSearchParams: jest.fn(),
+}));
+
+// Mock the behavior of useSearchParams
+const mockSearchParams = {
+  get: jest.fn(),
+};
+
+(useSearchParams as jest.Mock).mockReturnValue(mockSearchParams);
+
+describe('IdpListOnApp', () => {
+  test('should render IdpListOnApp component correctly', async () => {
+    mockSearchParams.get.mockReturnValue(null);
+    await renderWithProviders(<IdpListOnApp />);
+    const component = screen.getByText(it.ioesco.common.youridentityonio);
+    expect(component).toBeInTheDocument();
+  });
+});
