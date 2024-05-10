5f369e66a42fd2aa10ecd5a4e8adfe67737205fa
Claudio Dallospedale <{ID}+{username}@users.noreply.github.com>
2024-05-10 16:49:25 +0200

    Adding Email Validatoin Component

---
+++ src/app/[locale]/_component/emailValidationContainer/emailValidationContainer.tsx
@@ -0,0 +1,98 @@
+import { Box, Button, Grid, Typography } from '@mui/material';
+import useLocalePush from '../../_hooks/useLocalePush';
+
+type ButtonProps = {
+  href?: string;
+  text: string;
+  variant: 'outlined' | 'contained' | 'text' | 'naked';
+  onClick?: () => void;
+};
+
+type IntroductionProps = {
+  icon?: JSX.Element;
+  title: string;
+  subtitle?: string;
+  summary: string | JSX.Element | React.ReactNode;
+  button: ButtonProps;
+};
+
+export function EmailValidationContainer({
+  icon,
+  title,
+  subtitle,
+  summary,
+  button,
+}: IntroductionProps) {
+  const pushWithLocale = useLocalePush();
+
+  return (
+    <Box sx={{ maxWidth: 500, margin: '0 auto' }}>
+      <Grid container>
+        <Grid item xs={12} textAlign={'center'} pb={2}>
+          {icon}
+        </Grid>
+        <Grid item xs={12} pb={2}>
+          <Typography
+            fontSize={28}
+            fontWeight={700}
+            py={0}
+            px={0}
+            color="textPrimary"
+            sx={{
+              textAlign: 'center',
+            }}
+          >
+            {title}
+          </Typography>
+        </Grid>
+        {subtitle && <Grid item xs={12} pb={2}>
+          <Typography
+            fontSize={16}
+            fontWeight={600}
+            py={0}
+            px={0}
+            color="textSecondary"
+            sx={{
+              textAlign: 'center',
+            }}
+          >
+            {subtitle}
+          </Typography>
+        </Grid>
+        }
+        <Grid item xs={12} pb={4}>
+          <Typography
+            variant="subtitle1"
+            fontSize={18}
+            fontWeight={400}
+            py={0}
+            px={0}
+            color="textPrimary"
+            sx={{
+              textAlign: 'center',
+            }}
+          >
+            {summary}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} textAlign={'center'}>
+          <Grid display={'flex'} justifyContent="center">
+            <Button
+              onClick={
+                () => {
+                      pushWithLocale(button.href || '/');
+                      if (button.onClick) {
+                        button.onClick();
+                      }
+                    }
+              }
+              variant={button.variant}
+            >
+              {button.text}
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
