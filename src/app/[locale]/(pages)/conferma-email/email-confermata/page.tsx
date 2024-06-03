3941d06e020e25d9fd120af4eb4f71b37cf456f4
Claudio Dallospedale <{ID}+{username}@users.noreply.github.com>
2024-06-03 22:23:18 +0200

    Adding EMAIL Confirmation Page

--- src/app/[locale]/(pages)/conferma-email/email-confermata/page.tsx
+++ src/app/[locale]/(pages)/conferma-email/email-confermata/page.tsx
@@ -1,25 +1,36 @@
 'use client';
 
 import { Grid } from '@mui/material';
-// import { useTranslations } from 'next-intl';
-import { commonBackgroundFullHeight } from '../../../_utils/styles';
+import { useTranslations } from 'next-intl';
+import { commonBackgroundLightFullHeight } from '../../../_utils/styles';
 import { EmailValidationContainer } from '../../../_component/emailValidationContainer/emailValidationContainer';
 import { IllusCompleted } from '@pagopa/mui-italia';
+import { backToIo } from '@/app/[locale]/_utils/common';
 
 const EmailConfirmedPage = (): React.ReactElement => {
-  // const t = useTranslations('ioesco');
-  
+  const t = useTranslations('ioesco');
+
   return (
-    <Grid sx={commonBackgroundFullHeight} container>
+    <Grid
+      sx={commonBackgroundLightFullHeight}
+      alignItems="center"
+      justifyContent="center"
+      container
+    >
       <Grid item xs={12} justifySelf={'center'} marginTop={5}>
-              <EmailValidationContainer
-            icon={<IllusCompleted />}
-            title={'Fatto!'}
-            subtitle={'Hai confermato la tua email! Torna su IO per continuare a usare l’app.'}
-            button={{
-                variant: 'contained',
-                text: 'Torna su IO',
-            }}
+        <EmailValidationContainer
+          icon={<IllusCompleted />}
+          title={
+            t.rich('emailvalidation.done', {
+              strong: chunks => <strong>{chunks}</strong>,
+            }) as string
+          }
+          subtitle={t('emailvalidation.emailconfirmed')}
+          button={{
+            variant: 'contained',
+            text: t('emailvalidation.backtoio'),
+            onClick: () => backToIo(1000),
+          }}
         />
       </Grid>
     </Grid>
