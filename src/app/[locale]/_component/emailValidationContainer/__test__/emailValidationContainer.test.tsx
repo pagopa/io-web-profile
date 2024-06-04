a87598c48abc4f8b11462e7da39848005af71684
Claudio Dallospedale <145718190+ClaudioNTT@users.noreply.github.com>
2024-06-04 09:16:59 +0200

    [IOPID-1557] : Email Confirmation  (#154)
    ## Short description
    
    This pull request aims to implement the email-validation page within the
    io web profile project, as outlined in the JIRA task.
    
    ## List of changes proposed in this pull request
    
    - Added EmailValidationApiClient
    - Added Email Confirmation page
    - Modified session provider and layout to accommodate pages without
    header and footer (in this case).
    
    ## How to test
    - To enable the email validation page, set the env variable
    **NEXT_PUBLIC_VALIDATION_EMAIL** to **true**
    - Set up the mock API using Mockoon for the io function.
    - Access the io web profile home page and verify functionality.
    - Enable Mockoon with the new mock (stored in the mock folder).
    - Access the email confirmation page using the provided token in the
    query parameter and verify API calls according to OpenAPI
    specifications.
    
    Example URL:
    http://localhost:3000/it/conferma-email/?token=05QSY3JXN8XF47LTKRW9EMHZBX:179aeae8dcc01abdab31e5ba
    
    
    # The KO pages and the management of the various errors will be
    introduced in the next PR since they are on other Jira tasks.
    
    Video:
    
    
    [Video](https://github.com/pagopa/io-web-profile/assets/145718190/8d2c8876-5b8f-49d9-bb76-2c09d97509b1)
    
    ---------
    
    Co-authored-by: Claudio Dallospedale <{ID}+{username}@users.noreply.github.com>
    Co-authored-by: Alice Di Rico <alice.dirico@outlook.it>
    Co-authored-by: Alice Di Rico <83651704+Ladirico@users.noreply.github.com>

---
+++ src/app/[locale]/_component/emailValidationContainer/__test__/emailValidationContainer.test.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { fireEvent, screen } from '@testing-library/react';
+import { EmailValidationContainer } from '../emailValidationContainer';
+import { renderWithProviders } from '@/app/[locale]/_utils/test-utils';
+import { vi } from 'vitest';
+
+describe('EmailValidationContainer component', () => {
+  const handleClick = vi.fn();
+
+  const defaultProps = {
+    icon: <div data-testid="top-icon" />,
+    title: 'Test Title',
+    summary: 'Test Summary',
+    button: {
+      text: 'FirstButton',
+      variant: 'outlined' as const,
+      onClick: handleClick,
+    },
+  };
+
+  test('should render properly', async () => {
+    await renderWithProviders(<EmailValidationContainer {...defaultProps} />);
+
+    expect(await screen.findByTestId('top-icon')).toBeInTheDocument();
+    expect(screen.getByText('Test Title')).toBeInTheDocument();
+    expect(screen.getByText('Test Summary')).toBeInTheDocument();
+    expect(await screen.findByText('FirstButton')).toBeInTheDocument();
+  });
+
+  test('should trigger onClick when the button is clicked', async () => {
+    await renderWithProviders(<EmailValidationContainer {...defaultProps} />);
+    
+    const button = await screen.findByText('FirstButton');
+    fireEvent.click(button);
+
+    expect(handleClick).toHaveBeenCalled();
+  });
+
+});
