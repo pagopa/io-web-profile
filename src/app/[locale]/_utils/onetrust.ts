0dee62369bb09d99556a9daf57080ef16083217c
Alice Di Rico <83651704+Ladirico@users.noreply.github.com>
2025-07-15 09:35:51 +0200

    chore: [IOPID-3269] bump next version from 13.4.9 to 14.2.30 (#210)
    > [!Note]
    > This PR includes the Next.js version update originally proposed by
    Renovate in a [separate
    PR](https://github.com/pagopa/io-web-profile/pull/209). That previous
    Renovate PR will be automatically closed once this one is merged.
    
    ## Short description
    Bump Next version from `13.4.9` to `14.2.30`, and add minor fix
    
    ## List of changes proposed in this pull request
    1. Upgrade to version `14.2.30`
    2. Updated the pull request template (added “demo” section and
    eliminated unnecessary spaces)
    3. Updated API call mocks on mockoon (merged all mocks into one file and
    added mocks for local login)
    4. Updated the .env files with the new configurations
    5. corrected cookie handling in local and production environments.
    (cookie initialization was done more than once before)
    6. Updated the README instructions
    7. Added `div` component as wrapper of `typography` component in Faq
    component in order to avoid console warnings
    
    ## Demo
    <details><summary>email validation link flow</summary>
    <p>
    
    <video
    src="https://github.com/user-attachments/assets/c7ecb27a-0c14-431c-a271-11c063613772"/>
    
    </p>
    </details>
    
    <details><summary>magic link flow</summary>
    <p>
    
    <video
    src="https://github.com/user-attachments/assets/9763d082-3c09-4792-9f97-f6593d95de09"/>
    
    </p>
    </details>
    
    <details><summary>classic flow (login, logout from IO, wallet and lock
    session)</summary>
    <p>
    
    <video
    src="https://github.com/user-attachments/assets/7ef01e5e-b497-46fe-8abd-f58ff35557ae"/>
    
    </p>
    </details>
    
    ## How to test
    1. Start the application with `yarn dev` and verify that OneTrust
    cookies are handled properly and that no errors appear in the console.
    (to show the cookie check that there are no cookies saved in the
    `inspector > cookies`, if there are provide to delete them)
    2. Build and run the application with `yarn build:prod && yarn
    start-static` and verify that cookies are working properly and no errors
    appear.
    3. Verify that the navigation and main functions of the application are
    working as expected. (as you can see from the demo)
    5. Verify that the mooks are up to date and working.
    6. Confirm that the new values in the .env files are correct and used by
    the application.
    7. Confirm that the new pull request template is used correctly.
    8. Confirm that the readme.md file is well defined and correct.

--- src/app/[locale]/_utils/onetrust.ts
+++ src/app/[locale]/_utils/onetrust.ts
@@ -23,11 +23,22 @@ export const COOKIE_NOTICE_ID = isDevMode()
   ? `${process.env.NEXT_PUBLIC_ONETRUST_COOKIES_CONSENT_OTNOTICE_ID}-test`
   : `${process.env.NEXT_PUBLIC_ONETRUST_COOKIES_CONSENT_OTNOTICE_ID}`;
 
+/**
+ * Initializes the OneTrust cookie notice script by dynamically injecting it into the document head.
+ *
+ * The function first checks if a script element with the specific `data-domain-script` attribute
+ * (matching `COOKIE_NOTICE_ID`) already exists in the DOM. This check is necessary to prevent
+ * multiple insertions of the OneTrust script, which could lead to duplicate initialization,
+ * unexpected behavior, or performance issues. If the script is not present, it creates and appends
+ * the script element to load the OneTrust SDK.
+ */
 export function initOneTrust() {
-  const scriptEl = document.createElement('script');
-  scriptEl.setAttribute('src', '/onetrust/scripttemplates/otSDKStub.js');
-  scriptEl.setAttribute('type', 'text/javascript');
-  scriptEl.setAttribute('charset', 'UTF-8');
-  scriptEl.setAttribute('data-domain-script', COOKIE_NOTICE_ID);
-  document.head.appendChild(scriptEl);
+  if (!document.querySelector(`script[data-domain-script="${COOKIE_NOTICE_ID}"]`)) {
+    const scriptEl = document.createElement('script');
+    scriptEl.setAttribute('src', '/onetrust/scripttemplates/otSDKStub.js');
+    scriptEl.setAttribute('type', 'text/javascript');
+    scriptEl.setAttribute('charset', 'UTF-8');
+    scriptEl.setAttribute('data-domain-script', COOKIE_NOTICE_ID);
+    document.head.appendChild(scriptEl);
+  }
 }
